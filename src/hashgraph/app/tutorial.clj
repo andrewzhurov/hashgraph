0cc89e18e0ffca4fa3246bc2964ba81592184015
Andrew Zhurov <zhurov.andrew@gmail.com>
2024-02-25 16:18:36 +0300

    Fix highlight of inspected tutor inspectables

--- src/hashgraph/app/tutorial.clj
+++ src/hashgraph/app/tutorial.clj
@@ -1,7 +1,7 @@
 (ns hashgraph.app.tutorial
   (:require [hashgraph.app.inspector :as hga-inspector]
-            #_[hashgraph.utils.core :refer [hash=]]))
+            [hashgraph.utils.core :as utils]))
 
 (defmacro i [el el-name & more]
-  `(hga-inspector/inspectable-el ~el {:->inspected? (fn [ips# val#] (and (= 1 (count ips)) (hash= (first ips) val)))}
+  `(hga-inspector/inspectable-el ~el {:->inspected? (fn [ips# val#] (and (= 1 (count ips#)) (utils/hash= (first ips#) val#)))}
                                  [:div.ref ~el-name ~@more]))
