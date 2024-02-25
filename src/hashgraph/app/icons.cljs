f591719c44664abd6755b496f26ab2557edbe0b3
Andrew Zhurov <zhurov.andrew@gmail.com>
2024-02-25 15:24:47 +0300

    Add svg-fa util

--- src/hashgraph/app/icons.cljs
+++ src/hashgraph/app/icons.cljs
@@ -6,11 +6,16 @@
             ["@fortawesome/fontawesome-svg-core" :refer [library] :as fa-core]
             [goog.object]))
 
-(defn transfer [{:keys [size style]}]
-  [:svg.svg-inline--fa {:xmlns   "http://www.w3.org/2000/svg",
-                        :viewBox "0 0 48 48"
-                        :class   (str "fa-" size)
-                        :fill    (:color style)}
+(defn svg-fa [{:keys [size style]} & content]
+  (into [:svg.svg-inline--fa {:xmlns   "http://www.w3.org/2000/svg",
+                              :viewBox "0 0 48 48"
+                              :class   (str "fa-" size)
+                              :fill    (:color style)}]
+        content))
+
+(defn transfer [opts]
+  (svg-fa
+   opts
    [:title "transfer"]
    [:g {:style {:transform-origin :center
                 :scale            "0.9"}}
@@ -25,7 +30,7 @@
        "M43,26a2,2,0,0,0-2,2V40H31V28a2,2,0,0,0-4,0V42a2,2,0,0,0,2,2H43a2,2,0,0,0,2-2V28A2,2,0,0,0,43,26Z"}]
      [:path
       {:d
-       "M34,16v1.2l-2.6-2.6a1.9,1.9,0,0,0-3,.2,2.1,2.1,0,0,0,.2,2.7l6,5.9a1.9,1.9,0,0,0,2.8,0l6-5.9a2.1,2.1,0,0,0,.2-2.7,1.9,1.9,0,0,0-3-.2L38,17.2V16a14,14,0,0,0-28,0v6a2,2,0,0,0,4,0V16a10,10,0,0,1,20,0Z"}]]]])
+       "M34,16v1.2l-2.6-2.6a1.9,1.9,0,0,0-3,.2,2.1,2.1,0,0,0,.2,2.7l6,5.9a1.9,1.9,0,0,0,2.8,0l6-5.9a2.1,2.1,0,0,0,.2-2.7,1.9,1.9,0,0,0-3-.2L38,17.2V16a14,14,0,0,0-28,0v6a2,2,0,0,0,4,0V16a10,10,0,0,1,20,0Z"}]]]))
 
 (defn icon-key->icon-name [icon-key]
   (apply str "fa" (-> (name icon-key)
