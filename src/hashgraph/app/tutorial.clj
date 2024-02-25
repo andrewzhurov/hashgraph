86f529a42790eff6bd70e4edc99368b5fa9c82a1
Andrew Zhurov <zhurov.andrew@gmail.com>
2024-02-25 14:34:17 +0300

    Adjust tutor inspectables to be highlighted on exact match

--- src/hashgraph/app/tutorial.clj
+++ src/hashgraph/app/tutorial.clj
@@ -3,5 +3,5 @@
             #_[hashgraph.utils.core :refer [hash=]]))
 
 (defmacro i [el el-name & more]
-  `(hga-inspector/inspectable-el ~el {:->inspected? (fn [ips# val#] (hga-inspector/->in ips# val#))}
+  `(hga-inspector/inspectable-el ~el {:->inspected? (fn [ips# val#] (and (= 1 (count ips)) (hash= (first ips) val)))}
                                  [:div.ref ~el-name ~@more]))
