921c959795fd57f4e56afe66bb8aec1d0d13c07f
Andrew Zhurov <zhurov.andrew@gmail.com>
2024-02-25 13:25:13 +0300

    Fix debugger overflowing tutors on z-axis

--- src/hashgraph/app/view.cljs
+++ src/hashgraph/app/view.cljs
@@ -12,10 +12,9 @@
 (def issue-link "https://github.com/andrewzhurov/hashgraph/issues")
 (def discussions-link "https://github.com/andrewzhurov/hashgraph/discussions")
 
-(def tutorial-z 1)
-(def debug-inspector-z 2)
-(def viz-z 2)
-
+(def debug-inspector-z 1)
+(def tutorial-z 2)
+(def viz-z 3)
 
 (def border-radius 6)
 (def tutorial-size 300)
